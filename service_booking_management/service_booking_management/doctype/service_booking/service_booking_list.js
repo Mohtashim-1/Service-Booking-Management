1c4eb16d5b23d9ab3ac399c21b1229f4152c6a2b
Mohtashim-1 <shoaibmohtashin973@gmail.com>
2025-06-23 13:52:10 +0500

    fix: fix in booking list and service booking

--- service_booking_management/service_booking_management/doctype/service_booking/service_booking_list.js
+++ service_booking_management/service_booking_management/doctype/service_booking/service_booking_list.js
@@ -1,14 +1,4 @@
-// Copyright (c) 2025, mohtashim and contributors
-// For license information, please see license.txt
 
-// frappe.ui.form.on("Service Booking", {
-// 	refresh(frm) {
-
-// 	},
-// });
-
-
-// service_booking_list.js
 frappe.listview_settings['Service Booking'] = {
     // ensure status is fetched
     add_fields: ['status'],
